d1ff204d19c10b33718695c93815e78e01c64abe
imsherr <imsherrill@gmail.com>
2024-05-10 10:25:34 -0400

    add data attribute

--- src/components/Node/Node.tsx
+++ src/components/Node/Node.tsx
@@ -210,6 +210,7 @@ const Node = ({
       data-flume-component="node"
       data-flume-node-type={currentNodeType.type}
       data-flume-component-is-root={!!root}
+      data-flume-has-appendage={!!appendage}
       onContextMenu={handleContextMenu}
       stageState={stageState}
       stageRect={stageRect}
